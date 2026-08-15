1bc83196a3db6431b22e9659507a8dc88c292434
Maks Solomon <maks.solomon2@gmail.com>
2026-08-15 09:45:31 +0200

    render seats

--- src/components/CinemaSeatBooking.jsx
+++ src/components/CinemaSeatBooking.jsx
@@ -3,7 +3,7 @@ import React, { useState, useMemo } from 'react'
 const CinemaSeatBooking = ({
   layout= {
     rows: 8,
-    setPerRow: 12,
+    seatsPerRow: 12,
     aislePosition: 5,
   },
   seatTypes = {
@@ -49,6 +49,31 @@ const CinemaSeatBooking = ({
 
   const [seats, setSeats] = useState(initializeSeats);
 
+  const getSeatClassName = (seat) => {
+    return "w-8 h-8 sm:w-10 sm:h-10 lg:w-12 m-1 rounded-t border-2 cursor-pointer transition-all duration-200 flex justify-center items-center text-xs sm:text-sm font-bold border-blue-300 text-blue-800 bg-blue-200";
+  };
+
+  const renderSeatSection = (seatRow, startIndex, endIndex) => {
+    return ( 
+      <div className='flex'>
+        {seatRow.slice(startIndex, endIndex).map((seat, index) => {
+          return ( 
+            <div 
+              className={getSeatClassName(seat)} 
+              key={seat.id}
+              title={ `${seat.id} - ${
+                getSeatType(seat.row)?.name || "Regular"
+              } - ${currency}${seat.price} `}
+            >
+                {" "}
+                {startIndex + index + 1}
+            </div>
+          );
+        })}
+      </div>
+    )
+  };
+
   return (
     <div className='w-full text-center'>
       {/* tiltle */}
@@ -59,19 +84,27 @@ const CinemaSeatBooking = ({
     
       {/* Screen */}
       <div>
-        <div className='mx-auto w-[60%] h-4 bg-gray-300 mt-10 mb-2 rounded-2xl shadow-inner'/>
+        <div className='mx-auto w-[90%] h-4 bg-gray-300 mt-10 mb-2 rounded-2xl shadow-inner'/>
           <p className='text-center'>SCREEN</p>    
       </div>
 
       {/* Seat Map */}
       <div className='mb-6 overflow-x-auto'>
-        <div className='flex flex-col items-center min-w-max '>
+        <div className='flex flex-col items-center min-w-max mt-20 '>
           {seats.map((row, rowIndex) => {
             return (
-              <div key={rowIndex} className='flex'>
-                <span className='w-8 text-center font-bold mr-4 text-white'>
+              <div key={rowIndex} className='flex mt-3'>
+                <span className='mt-2 h-8 w-8 text-center font-bold mr-4 text-white border-2 border-gray-500 bg-gray-800'>
                   {String.fromCharCode(65 + rowIndex)} 
                 </span>
+
+                {renderSeatSection(row, 0, layout.aislePosition)}
+
+                {renderSeatSection(
+                  row,
+                  layout.aislePosition,
+                  layout.setPerRow
+                )}
               </div>
             );
           })}
